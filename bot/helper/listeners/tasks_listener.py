8d8876c180a4a7ccb69221ee4b63ae6f7015b915
Softfreakz <65111266+Softfreakz@users.noreply.github.com>
2024-03-09 09:01:04 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -487,7 +487,6 @@ class MirrorLeechListener:
                 elif not rclonePath:
                     INDEX_URL = self.index_link if self.drive_id else config_dict['INDEX_URL']
                     if INDEX_URL:
-                      url_path = url_quote(f'{name}')
                         share_url = f"{INDEX_URL}findpath?id={dir_id}"
                         if mime_type == "Folder":
                             buttons.ubutton("📁 Direct Link", share_url)
